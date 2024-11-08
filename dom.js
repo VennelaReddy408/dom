a2fe60f19c114414b00f2c0af770bb2533344f4d
VennelaReddy408 <podduturivennela@gmail.com>
2024-11-08 10:33:20 +0530

    Add files via upload

---
+++ dom.js
@@ -0,0 +1,35 @@
+var k=document.getElementById("p")
+function b(){
+k.style.backgroundColor="red"
+k.style.color="white"
+k.style.fontSize="60px"
+k.style.height="300px"
+k.style.weight="200px"
+k.style.display="inline"
+k.style.borderRadius="50%"
+console.log(k)}
+
+// task2
+function c(){
+let images=document.getElementById("div");
+let button=document.getElementById("btn2");
+
+button.style.width="100px"
+
+images.innerHTML=`<img id="i1" src="https://rukminim2.flixcart.com/image/850/1000/kgsb1jk0-0/poster/n/h/y/medium-twfnp2-beautiful-waterfall-nature-view-large-size-high-original-imafwy37qv2b5g3v.jpeg?q=90&crop=false" width="200px"><img  id="i2" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_kmx5oAlTLw4YMHwM_YEaPdEYE1l1Fr0NCQ&s" width="200px"><img  id="i3" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTbpdV5eKcBMzjs7ltDev4YxvWvnNxcFk7wUg&s" width="200px">`
+
+let image1=document.getElementById("i1")
+image1.style.borderRadius="30px"
+image1.style.width="280px"
+let image2=document.getElementById("i2")
+image2.style.borderRadius="100px"
+image2.style.width="180px"
+let image3=document.getElementById("i3")
+image3.style.borderRadius="50%"
+image3.style.width="220px"
+}
+
+
+    
+ 
+    
